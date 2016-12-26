1872fc87b3b8988daf6e335fa26253de1ce5bfcc
Chu Kim Thang <chukimthang94@gmail.com>
2016-12-26 22:00:29 +0700

    Layout admin page

---
+++ resources/views/layouts/admin/menu.blade.php
@@ -0,0 +1,11 @@
+<div class="navbar-default sidebar" role="navigation">
+    <div class="sidebar-nav navbar-collapse">
+        <ul class="nav" id="side-menu">
+            <li>
+                <a href="#">
+                    @lang('text.menu_item_category')<span class="fa arrow"></span>
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
